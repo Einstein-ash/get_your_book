91d452f85404d2889360c3c9412ea5569b17581a
Ashwani <ashwanisingh46572@gmail.com>
2025-04-15 23:00:53 +0530

    will accept image url both

--- index.js
+++ index.js
@@ -152,6 +152,14 @@ const bookSchema = new mongoose.Schema({
         // required: true,
         trim: true
     }, 
+    orignalImageURL : {
+        type: String,
+        trim: true
+    }, 
+    invertedImageURL : {
+        type: String,
+        trim: true
+    }, 
     shelfNumber: {
         type: Number,
         // required: true,
@@ -183,14 +191,14 @@ app.get("/books", async (req, res) => {
 
 
 app.post("/books/", async (req, res) => {
-    const { bookName , userName, userUPI} = req.body;
+    const { bookName , userName, userUPI, orignalImageURL, invertedImageURL} = req.body;
 
     if (!bookName) {
         return res.status(400).send("bookName is required");
     }
 
     try {
-        const newBook = new Book({ bookName, userName, userUPI });
+        const newBook = new Book({ bookName, userName, userUPI, orignalImageURL, invertedImageURL });
         await newBook.save();
 
         res.status(201).send(`Book '${bookName}' added successfully`);
